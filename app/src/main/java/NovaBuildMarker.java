d604dcf23f6a1615fc14d7ed5793576412dcf43f
anantskr01 <anantswarnkar01@gmail.com>
2026-09-05 18:18:10 +0530

    Mark provider and language pipeline milestone

---
+++ app/src/main/java/NovaBuildMarker.java
@@ -0,0 +1,7 @@
+package com.aircontrol;
+
+/** Marker for the master-build architecture milestone. */
+public final class NovaBuildMarker {
+    public static final String VERSION = "master-build-v1";
+    private NovaBuildMarker() { }
+}
